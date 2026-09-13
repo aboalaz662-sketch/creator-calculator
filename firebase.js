79c46bb080eaccdfb1e49e6bb329b18658c1053c
aboalaz662-sketch <abo.alaz662@gmail.com>
2026-09-14 00:27:57 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,23 +7,14 @@ const firebaseConfig = {
   apiKey: "AIzaSyDWG6LUvXrx8IDKMQoTUWRVNhRiLhDEIY8",
   authDomain: "elaf-academy.firebaseapp.com",
   projectId: "elaf-academy",
-  storageBucket: "elaf-academy.firebasestorage.app",
+  storageBucket: "elaf-academy.appspot.com",
   messagingSenderId: "577219194497",
   appId: "1:577219194497:web:de693b30933b577799a780",
   measurementId: "G-YY8Y9VDYGQ"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-
-let analytics = null;
-if (typeof window !== "undefined") {
-  try {
-    analytics = getAnalytics(app);
-  } catch (e) {
-    console.warn("Analytics not initialized:", e);
-  }
-}
-
 export { app, analytics, db, storage };
